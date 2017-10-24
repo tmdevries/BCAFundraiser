7f6c678e2b1487bd9d01916d7b843d2136ddb817
tmdevries <tmdevries@cox.net>
2017-10-24 00:38:57 -0700

    Adding password reset feature

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def outbid
+    user = User.first
+    item = Item.first
+    UserMailer.outbid_email(user, item)
+  end	
 
+  def password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
+  end
 end
